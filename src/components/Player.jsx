b4ed636a1b33dcedd294f63adb537b136245e1ac
tanvir35web <islammdtanvirul@gmail.com>
2023-12-14 17:26:07 +0600

    prettier applied

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,30 +1,32 @@
 import { useState } from "react";
 
-export default function Player({initialName, symbol}) {
+export default function Player({ initialName, symbol }) {
   const [playerName, setPlayerName] = useState(initialName);
-    const [isEditing, setIsEditing] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
 
-    function handleEditClick () {
-        // setIsEditing(!isEditing);  This both are same.But Below code is more cleaner and best practices.
-        setIsEditing((editing) => !editing);
-    }
+  function handleEditClick() {
+    // setIsEditing(!isEditing);  This both are same.But Below code is more cleaner and best practices.
+    setIsEditing((editing) => !editing);
+  }
 
-    function handleChange(event) {
-      setPlayerName(event.target.value);
-    }
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
 
-    let editablePlayerName = <span className="player-name">{playerName}</span>
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
 
-    if(isEditing) {
-      editablePlayerName = <input type="text" required value={playerName} onChange={handleChange}/>
-    }
+  if (isEditing) {
+    editablePlayerName = (
+      <input type="text" required value={playerName} onChange={handleChange} />
+    );
+  }
   return (
     <li>
       <span className="player">
         {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button onClick={handleEditClick}>{isEditing ? 'SAVE' : 'EDIT'}</button>
+      <button onClick={handleEditClick}>{isEditing ? "SAVE" : "EDIT"}</button>
     </li>
   );
 }
